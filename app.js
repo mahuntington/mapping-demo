488f3f9d4c0baf48e1bcacf68319a0937d264e34
Matt Huntington <matt.huntington@gmail.com>
2022-09-13 08:56:53 -0400

    draw polygon

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ d3.select('svg').selectAll('path')
 	.append('path')
 	.attr('fill', '#099')
 	.attr('d', dAttributeFunction);
+
+const points = [];
+d3.select('svg').on('click', (event)=>{
+	points.push([event.x, event.y])
+	const path = d3.path();
+	path.moveTo(points[0][0], points[0][1])
+	for(let i = 1; i < points.length; i++){
+		path.lineTo(points[i][0], points[i][1])
+	}
+	path.closePath()
+	d3.select('svg')
+		.append('path')
+		.attr('id', 'polygon')
+		.attr('fill', 'black')
+		.attr('d', path)
+})
